331110e73eb3a3acb987e74e66844d037cbadb74
oxmerna <59712247+wmerna@users.noreply.github.com>
2024-06-09 03:42:28 +0200

    Update main.js - POSITIONING ISSUE

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ function test2() {
     global.counter = [1,1];
     global.doc = this;
     global.spacing = 40;
-    global.pageHeight = global.doc.getPageBox("Crop", 0).height;
-    // console.println(global.pageHeight);
     global.firstSection = { a: 100, b: 700, c: 200, d: 720 };
     global.secondSection = { a: 100, b: 600, c: 200, d: 620 };
     global.currentPage = [0,0]
@@ -38,9 +36,9 @@ function test2() {
         }
         var newTitlePos = [global.firstSection.a, currentY, global.firstSection.c, currentY + 20];
         var newEntryPos = [global.firstSection.a, currentY - 20, global.firstSection.c, currentY];
-        var newTitleField = global.doc.addField("SectionTitle" + global.counter[0], "text", global.currentPage[0], newTitlePos);
+        var newTitleField = global.doc.addField("SectionTitle" + global.counter[0] + "" + global.currentPage[0], "text", global.currentPage[0], newTitlePos);
         newTitleField.value = "This is a duplicated Field";
-        var newEntryField = global.doc.addField("EntryField" + global.counter[0], "text", global.currentPage[0], newEntryPos);
+        var newEntryField = global.doc.addField("EntryField" + global.counter[0]+ "" + global.currentPage[0], "text", global.currentPage[0], newEntryPos);
         newEntryField.value = "";
         global.counter[0] += 1;
         global.counter[1] += 1;
@@ -48,33 +46,61 @@ function test2() {
     });
 
     global.doc.addScript("deleteLastSection", function deleteLastSection() {
-        if (global.counter[0] > 1) {
+        console.println(global.counter);
+        if (global.counter[0] >= 1) {
             global.counter[0] -= 1;
             global.counter[1] -= 1;
-            var titleField = global.doc.getField("SectionTitle" + global.counter[0]);
+                if(global.counter[0] === 0 && global.currentPage[0] !== 0){
+                    console.println("the global counter is set at 0");
+                    global.counter[0] = 17;
+                    global.currentPage[0] -= 1; 
+                }
+            var titleField = global.doc.getField("SectionTitle" + global.counter[0] + "" + global.currentPage[0]);
             if (titleField != null) {
                 global.doc.removeField(titleField.name);
             }
-            var entryField = global.doc.getField("EntryField" + global.counter[0]);
+            var entryField = global.doc.getField("EntryField" + global.counter[0]+ "" + global.currentPage[0]);
             if (entryField != null) {
                 global.doc.removeField(entryField.name);
             }
-            adjustSecondSection(global.doc.numPages - 1);
+            adjustSecondSection();
         }
     });
 
     global.doc.addScript("adjustSecondSection", function adjustSecondSection() {
         var newSecondTitlePos = [global.secondSection.a, global.secondSection.b - (global.counter[1] * global.spacing), global.secondSection.c, global.secondSection.d - (global.counter[1] * global.spacing)];
         var newSecondEntryPos = [global.secondSection.a, global.secondSection.b - 20 - (global.counter[1] * global.spacing), global.secondSection.c, global.secondSection.d - 20 - (global.counter[1] * global.spacing)];
-        console.println(newSecondEntryPos[1] + "New secondEntry position when adjusting");
+
 
         
-        if(newSecondEntryPos[1] < 0) {
+        if(newSecondEntryPos[1] > 580 ) {
+
+        global.currentPage[1] -= 1;
+        global.counter[1] = 14;
+        newSecondTitlePos = [global.secondSection.a, global.secondSection.b - (global.counter[1] * global.spacing), global.secondSection.c, global.secondSection.d - (global.counter[1] * global.spacing)];
+        newSecondEntryPos = [global.secondSection.a, global.secondSection.b - 20 - (global.counter[1] * global.spacing), global.secondSection.c, global.secondSection.d - 20- (global.counter[1] * global.spacing)];
+        var index = global.currentPage[1] + 1
+        var secondTitleField = global.doc.getField("SecondSectionTitle" + index);
+        var secondEntryField = global.doc.getField("SecondEntryField" + index);
+
+        if (secondTitleField != null) {
+            var newSecondEntryTitle = global.doc.addField("SecondSectionTitle" + global.currentPage[1],"text",global.currentPage[1],newSecondTitlePos);
+            newSecondEntryTitle.value = secondTitleField.value;
+            global.doc.removeField(secondTitleField.name);
+
+            var newSecondEntryField = global.doc.addField("SecondEntryField" + global.currentPage[1],"text",global.currentPage[1],newSecondEntryPos);
+            newSecondEntryField.value = secondEntryField.value;
+            global.doc.removeField(secondEntryField.name);
+    }
+    return;
+
+    }
+        else if(newSecondEntryPos[1] < 0) {
 
             global.currentPage[1] += 1;
             global.counter[1] = 1;
-            newSecondTitlePos = [global.secondSection.a, global.secondSection.b, global.secondSection.c, global.secondSection.d];
-            newSecondEntryPos = [global.secondSection.a, global.secondSection.b - 20, global.secondSection.c, global.secondSection.d - 20];
+            newSecondTitlePos = [global.secondSection.a, global.secondSection.b - 40 - (global.counter[1] * global.spacing), global.secondSection.c, global.secondSection.d - 40 - (global.counter[1] * global.spacing)];
+            newSecondEntryPos = [global.secondSection.a, global.secondSection.b - 20 - (global.counter[1] * global.spacing), global.secondSection.c, global.secondSection.d - 20- (global.counter[1] * global.spacing)];
 
             var index = global.currentPage[1] - 1
             var secondTitleField = global.doc.getField("SecondSectionTitle" + index);
@@ -85,20 +111,19 @@ function test2() {
                 var newSecondEntryTitle = global.doc.addField("SecondSectionTitle" + global.currentPage[1],"text",global.currentPage[1],newSecondTitlePos);
                 newSecondEntryTitle.value = secondTitleField.value;
                 global.doc.removeField(secondTitleField.name);
-                console.println("this is the value of my field " + getField("SecondSectionField" + index));
-    
+
                 var newSecondEntryField = global.doc.addField("SecondEntryField" + global.currentPage[1],"text",global.currentPage[1],newSecondEntryPos);
                 newSecondEntryField.value = secondEntryField.value;
                 global.doc.removeField(secondEntryField.name);
-                console.println(secondEntryField);
             }
+
+            return;
 
         } else {
             var secondTitleField = global.doc.getField("SecondSectionTitle" + global.currentPage[1]);
             if (secondTitleField != null) {
                 secondTitleField.page = global.currentPage[1];
                 secondTitleField.rect = newSecondTitlePos;
-                console.println(global.currentPage + " Global Current Page when adjusting second section")
             }
             var secondEntryField = global.doc.getField("SecondEntryField" + global.currentPage[1]);
             if (secondEntryField != null) {
